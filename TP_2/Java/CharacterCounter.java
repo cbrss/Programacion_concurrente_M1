13abc1d08812760d958c50d82375ddd1bd3f011d
cbrss <cberrioslima@alumno.unlam.edu.ar>
2024-09-14 12:05:31 -0300

    refactor: TP2 java

--- TP_2/Java/CharacterCounter.java
+++ TP_2/Java/CharacterCounter.java
@@ -1,7 +1,7 @@
 import java.util.List;
-import java.util.ArrayList;
 
 public class CharacterCounter implements Runnable {
+	private static final int TIME_SLEEP = 1000;
 	private List<String> lines;
 	private int rp;
 	private int start;
@@ -18,17 +18,22 @@ public class CharacterCounter implements Runnable {
 		return this.rp;
 	}
 
-	public void count_characters() throws InterruptedException{
+	public void count_characters() {
 		int count = 0;
 		String line = "";
 
-		for (int i = this.start; i < this.end; i++){	// lineas
+		for (int i = this.start; i < this.end; i++){	// Itero lineas
 			line = this.lines.get(i);
-			for (int j = 0; j < line.length(); j++){		// cada linea
+			for (int j = 0; j < line.length(); j++){		// Itero caracteres de la linea
 				count++;
 			}
 		}
-		Thread.sleep(1000);
+		try{
+			Thread.sleep(TIME_SLEEP);
+		} catch(InterruptedException e){
+			e.printStackTrace();
+		}
+		
 		this.rp = count;
 	}
 
--- TP_2/Java/Main.java
+++ TP_2/Java/Main.java
@@ -2,84 +2,59 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
-import java.io.IOException;
 
 // hay que compilar javac CharacterCounter.java
 
 public class Main {
+
+    
 	public static void main(String[] args) {
         /// Valido los parametros
-		int total_threads = 0;
-        String file_path = "";
+        if (!validate_parameters(args)) {
+            return;
+        }
+        /// asigno variables
+		int total_threads = Integer.parseInt(args[1]);
+        String file_path = args[0];
 
-		if (args.length >2) {
-			System.out.println("Cantidad de parametros incorrecto.");
-			return;
-		} 
-		else {
-            file_path = args[0];
-            try{
-                total_threads = Integer.parseInt(args[1]);
-            } catch (NumberFormatException e) {
-                System.out.println("El 2do parametro tiene que ser un numero.");
-                return;
-            }
-			
-		}
-		if (total_threads < 1) {
-			System.out.println("La cantidad de threads tiene que ser igual o mayor a 1.");
-			return;
-		}
-
-        long t1 = System.currentTimeMillis();
-
-        /// Creo y ejecuto los hilos
+        /// Leo el archivo de entrada y relleno las lineas
         List<String> lines = new ArrayList<>();
-
         read_file(file_path, lines);
-        int cant_lines = 0;
-        cant_lines = lines.size();
-        if (total_threads > cant_lines) {   // Si la cantidad de hilos es mayor que la cantidad de lineas del archivo, entonces limito los hilos a la cantidad de lineas
-            total_threads = cant_lines;
-        }
-		
-		Thread[] threads = new Thread[total_threads];
-		CharacterCounter[] counters = new CharacterCounter[total_threads];
-		
-		int lines_per_thread = (cant_lines/total_threads);
-        int start = 0;
-        int end = 0;
-		for (int i = 0; i < total_threads; i++ ) {
-			start = i * lines_per_thread;
-            end = (i == total_threads -1)? cant_lines : start + lines_per_thread;
-            counters[i] = new CharacterCounter(lines, start, end);
-            threads[i] = new Thread(counters[i]);
-            threads[i].start();
-            System.out.println("Hilo: " + i + " ejecutando: " + start + " hasta " + end);
-		}
 
-        /// Libero recursos de los hilos
-        try{
-            for (int i = 0; i < total_threads; i++) {
-                threads[i].join();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        
-        /// Conteo final
-        int rt = 0;
-        for (CharacterCounter counter : counters) {
-            rt += counter.getRp();
-        }
-
+        /// Ejecucion del conteo y toma de tiempo
+        long t1 = System.currentTimeMillis();
+        int rt = execute_counter(lines, total_threads);
         long t2 = System.currentTimeMillis();
 
         long tt = (t2 - t1);
 
         System.out.println("Total: " + rt + " calculado en: " + tt + "ms");
 	}
+    public static boolean validate_parameters(String[] parameters) {
+        if (parameters.length > 2 || parameters.length < 1){
+            System.out.println("Cantidad de parametros incorrecto.");
+            return false;
+        }
+        File file = new File(parameters[0]);
+        if (!file.exists() || !file.isFile()){
+            System.out.println("El primer parametro debe ser un archivo.");
+            return false;
+        }
+        int total_threads;
+        try{
+            total_threads = Integer.parseInt(parameters[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("El 2do parametro tiene que ser un numero.");
+            return false;
+        }
+        if (total_threads < 1) {
+			System.out.println("La cantidad de threads tiene que ser igual o mayor a 1.");
+			return false;
+		}
 
+        return true;
+    }
+    
 	private static void read_file(String name, List<String> lines) {
 		File fp = null;
 		Scanner sc = null;
@@ -102,4 +77,47 @@ public class Main {
 			sc.close();
 		}
 	}
+    private static int execute_counter(List<String> lines, int total_threads){
+        
+        int total_lines = lines.size();
+
+        if (total_threads > total_lines) {   // Si la cantidad de hilos es mayor que la cantidad de lineas del archivo, entonces limito los hilos a la cantidad de lineas
+            total_threads = total_lines;
+        }
+		
+		Thread[] threads = new Thread[total_threads];
+		CharacterCounter[] counters = new CharacterCounter[total_threads];
+		
+		int lines_per_thread = (total_lines/total_threads);
+        int start = 0;
+        int end = 0;
+		for (int i = 0; i < total_threads; i++ ) {
+			start = i * lines_per_thread;
+            end = (i == total_threads -1)? total_lines : start + lines_per_thread;
+            counters[i] = new CharacterCounter(lines, start, end);
+            threads[i] = new Thread(counters[i]);
+            threads[i].start();
+            System.out.println("Hilo: " + i + " ejecutando: " + start + " hasta " + end);
+		}
+
+        /// Libero recursos de los hilos
+        try{
+            for (int i = 0; i < total_threads; i++) {
+                threads[i].join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        /// Conteo final
+        int rt = 0;
+        for (CharacterCounter counter : counters) {
+            rt += counter.getRp();
+        }
+
+
+        return rt;
+    }
 }
+    
+
